9eef2aba6d67dce574eb1207a8d3394da1fa4255
ja06muen <ja06muen@siena.edu>
2014-07-17 15:22:40 -0400

    condensing the code

--- top_quark_xsec/assigning_jets.py
+++ top_quark_xsec/assigning_jets.py
@@ -2,123 +2,13 @@ import numpy as np
 import matplotlib.pylab as plt
 import sys
 import math
-
+import decay_tools as d_tools
 import lichen.lichen as lkn
 import hep_tools_text_files as hep
 
 t0=[]       #top and decay 
-t1=[]       #antitop and decay
 
-
-def return_top_decays(pdgs):
-    #print pdgs
-    t0=[-999,-999,-999,-999,-999]       #top and decay 
-    t1=[-999,-999,-999,-999,-999]       #antitop and decay
-
-    found_top=False
-    found_antitop=False
-    
-    found_W=False
-    found_antiW=False
-    
-    found_b=False
-    found_antib=False
-    
-    found_decay_1=False
-    found_anti_decay_1=False
-    
-    found_decay_2=False
-    found_anti_decay_2=False
-
-    # To find the top and antitop
-    for i,pdg in enumerate(pdgs):
-        #print i,pdg
-        if pdg==6:
-            found_top=True
-            t0[0] = i
-        if pdg==-6:
-            found_antitop=True
-            t1[0] = i
-    
-    # To find the W and anti W
-    for i,pdg in enumerate(pdgs):
-        if pdg==24 and found_top==True:
-            found_W=True
-            t0[1] = i
-        if pdg==-24 and found_antitop==True:
-            found_antiW=True
-            t1[1] = i
-    # To find the b and anti b
-    for i,pdg in enumerate(pdgs):
-        if pdg==5 and found_W==True:
-            found_b=True
-            t0[2] = i
-        if pdg==-5 and found_antiW==True:
-            found_antib=True
-            t1[2] = i
-
-    #To find the first decay 
-    for i,pdg in enumerate(pdgs):
-        if found_decay_1==False and found_b==True and t0[2]<i:
-            if pdg==2 or pdg==4:
-                found_decay_1=True 
-                t0[3] = i
-            elif pdg==-11 or pdg==-13 or pdg==-15:
-                found_decay_1=True 
-                t0[3] = i
-                #t0[4]=0.0
-     #To find the first anti decay           
-        if found_anti_decay_1==False and found_antib==True and t1[2]<i:
-            if pdg==-2 or pdg==-4:
-                found_anti_decay_1=True
-                t1[3] = i
-            elif pdg==11 or pdg==13 or pdg==15:
-                found_anti_decay_1=True
-                t1[3] = i
-                #t1[4]=0.0
-    #To find the second decay     
-    for i,pdg in enumerate(pdgs):     
-        if found_decay_2==False and found_decay_1==True and t0[2]<i:        
-            if pdg==-1 or pdg==-3:
-                found_decay_2=True
-                t0[4] = i
-            elif pdg==12 or pdg==14 or pdg==16:
-                found_decay_2=True
-                t0[4] = i
-    #To find the second anti decay
-        if found_anti_decay_2==False and found_anti_decay_1==True and t1[2]<i:
-            if pdg==1 or pdg==3:
-                found_anti_decay_2=True
-                t1[4] = i 
-            elif pdg==-12 or pdg==-14 or pdg==-16:
-                found_anti_decay_2=True
-                t1[4] = i
-   
-    
-    
-    #print t0
-    #print t1
-    return[t0,t1]
-    
-    
-def delta_R(eta,phi,m,n):
-    
-    a=np.abs(phi[m]-phi[n])
-    while a > math.pi:
-        a=a-(2*math.pi)
-    b=eta[m]-eta[n]
-    
-    dR=np.sqrt((np.abs(a))**2+(np.abs(b))**2)
-    
-    return [dR,a,b]
-    
-def delta_R_jet(eta1,phi1,eta2,phi2):
-    a=np.abs(phi1-phi2)
-    while a > math.pi:
-        a=a-math.pi
-    
-    dR=np.sqrt((np.abs(a))**2+(np.abs(eta1-eta2))**2)
-    return dR      
+      
     
 
 f = open(sys.argv[1])
@@ -164,7 +54,7 @@ for collision in collisions:
         phis.append(phi)
         #print "%-5d %8.5f %12.5f %12.5f" % (pdg,pt,eta,phi)
 
-    t0=return_top_decays(pdgs)
+    t0=d_tools.return_top_decays(pdgs)
     
     ######################################### Top Jets
     
@@ -192,7 +82,7 @@ for collision in collisions:
 
         if abs(pdgs[t0[0][3]])<6:
             for i in range (16):
-                del_r.append(delta_R_jet(jet_eta[i],jet_phi[i],etas[t0[0][0]],phis[t0[0][0]]))
+                del_r.append(d_tools.delta_R_jet(jet_eta[i],jet_phi[i],etas[t0[0][0]],phis[t0[0][0]]))
 
 
             min_dR=(min(del_r))
@@ -210,7 +100,7 @@ for collision in collisions:
             
         if abs(pdgs[t0[1][3]])<6:
             for i in range (16):
-                del_r_anti.append(delta_R_jet(jet_eta[i],jet_phi[i],etas[t0[1][0]],phis[t0[1][0]]))
+                del_r_anti.append(d_tools.delta_R_jet(jet_eta[i],jet_phi[i],etas[t0[1][0]],phis[t0[1][0]]))
 
             min_dR=min(del_r_anti)
 
--- top_quark_xsec/read_text_file_with_jets.py
+++ top_quark_xsec/read_text_file_with_jets.py
@@ -2,116 +2,12 @@ import numpy as np
 import matplotlib.pylab as plt
 import sys
 import math
-
+import decay_tools as d_tools
 import lichen.lichen as lkn
 import hep_tools_text_files as hep
 
 t0=[]       #top and decay 
-t1=[]       #antitop and decay
-
-
-def return_top_decays(pdgs):
-    #print pdgs
-    t0=[-999,-999,-999,-999,-999]       #top and decay 
-    t1=[-999,-999,-999,-999,-999]       #antitop and decay
-
-    found_top=False
-    found_antitop=False
-    
-    found_W=False
-    found_antiW=False
-    
-    found_b=False
-    found_antib=False
-    
-    found_decay_1=False
-    found_anti_decay_1=False
-    
-    found_decay_2=False
-    found_anti_decay_2=False
-
-    # To find the top and antitop
-    for i,pdg in enumerate(pdgs):
-        #print i,pdg
-        if pdg==6:
-            found_top=True
-            t0[0] = i
-        if pdg==-6:
-            found_antitop=True
-            t1[0] = i
-    
-    # To find the W and anti W
-    for i,pdg in enumerate(pdgs):
-        if pdg==24 and found_top==True:
-            found_W=True
-            t0[1] = i
-        if pdg==-24 and found_antitop==True:
-            found_antiW=True
-            t1[1] = i
-    # To find the b and anti b
-    for i,pdg in enumerate(pdgs):
-        if pdg==5 and found_W==True:
-            found_b=True
-            t0[2] = i
-        if pdg==-5 and found_antiW==True:
-            found_antib=True
-            t1[2] = i
-
-    #To find the first decay 
-    for i,pdg in enumerate(pdgs):
-        if found_decay_1==False and found_b==True and t0[2]<i:
-            if pdg==2 or pdg==4:
-                found_decay_1=True 
-                t0[3] = i
-            elif pdg==-11 or pdg==-13 or pdg==-15:
-                found_decay_1=True 
-                t0[3] = i
-                #t0[4]=0.0
-     #To find the first anti decay           
-        if found_anti_decay_1==False and found_antib==True and t1[2]<i:
-            if pdg==-2 or pdg==-4:
-                found_anti_decay_1=True
-                t1[3] = i
-            elif pdg==11 or pdg==13 or pdg==15:
-                found_anti_decay_1=True
-                t1[3] = i
-                #t1[4]=0.0
-    #To find the second decay     
-    for i,pdg in enumerate(pdgs):     
-        if found_decay_2==False and found_decay_1==True and t0[2]<i:        
-            if pdg==-1 or pdg==-3:
-                found_decay_2=True
-                t0[4] = i
-            elif pdg==12 or pdg==14 or pdg==16:
-                found_decay_2=True
-                t0[4] = i
-    #To find the second anti decay
-        if found_anti_decay_2==False and found_anti_decay_1==True and t1[2]<i:
-            if pdg==1 or pdg==3:
-                found_anti_decay_2=True
-                t1[4] = i 
-            elif pdg==-12 or pdg==-14 or pdg==-16:
-                found_anti_decay_2=True
-                t1[4] = i
    
-    
-    
-    #print t0
-    #print t1
-    return[t0,t1]
-    
-    
-def delta_R(eta,phi,m,n):
-    
-    a=np.abs(phi[m]-phi[n])
-    while a > math.pi:
-        a=a-math.pi
-    
-    dR=np.sqrt((np.abs(a))**2+(np.abs(eta[m]-eta[n]))**2)
-    
-    return dR
-    
-    
     
 
 f = open(sys.argv[1])
@@ -139,11 +35,8 @@ for collision in collisions:
 
     gen_particles,ak5jets,ca8jets = hep.get_truth_particles_from_collision(collision)
 
-    #print gen_particles
-    #print ak5jets
-    #print ca8jets
 
-    #exit()
+
 
     #print "--------------------------"
     pdgs=[]
@@ -158,10 +51,7 @@ for collision in collisions:
         etas.append(eta)
         phis.append(phi)
         
-    t0=return_top_decays(pdgs)
-    #dR for top quarks
-    #dR=delta_R(etas,phis,t0[0][0],t0[1][0])
-    #del_R.append(dR)
+    t0=d_tools.return_top_decays(pdgs)
 
 
     muon=False
@@ -193,7 +83,7 @@ for collision in collisions:
                 anti_muon=True
                 muon_pt.append(pt)
                 
-    '''#finding the electrons pt
+    #finding the electrons pt
         if electron==False and status==3:
             if pdg==-11:
                 electron=True                
@@ -222,7 +112,7 @@ for collision in collisions:
         quarks_pt.append(pts[t0[1][3]])
         quarks_pt.append(pts[t0[1][4]])
 
-#print del_R
+
 #Finding top pt
     top_pt.append(pts[t0[0][0]])
     top_pt.append(pts[t0[1][0]])        
@@ -236,35 +126,6 @@ for collision in collisions:
     bottom_pt.append(pts[t0[1][2]])
     
     
-    ######################################### Delta R
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        #print "%-5d %-5d %8.5f %12.5f %12.5f" % (pdg,status,pt,eta,phi)
-
-    #print "-------"
-    #for p in ak5jets:
-        #mass,pt,eta,phi = p
-
-        #print "%8.5f %8.5f %12.5f %12.5f" % (mass,pt,eta,phi)
-
-    #print "-------"
-    #for p in ca8jets:
-        #mass,pt,eta,phi = p
-
-        #print "%8.5f %8.5f %12.5f %12.5f" % (mass,pt,eta,phi)
-
-
-
-
-
-
 
 
 
@@ -315,4 +176,4 @@ plt.figure (7)
 lkn.hist_err(quarks_pt,bins=100,range=(0,300))
 plt.title(r"Quark p$_T$")
 plt.xlabel(r"p$_T$(GeV/c)")
-plt.show()'''
+plt.show()
