2ae9d1757ba04f6eac0ee2e6e596c42917210d62
ja06muen <ja06muen@siena.edu>
2015-03-05 12:48:47 -0500

    Find W and Z Bosons

---
+++ aps_stuff/Find_W_and_Z.py
@@ -0,0 +1,96 @@
+import siena_cms_tools as cms
+import numpy as np
+import matplotlib.pylab as plt
+import math
+import sys
+import lichen.lichen as lkn
+
+
+
+f = open(sys.argv[1])
+
+invarient_mass_W=[]
+invarient_mass_Z=[]
+
+collisions = cms.get_collisions(f)
+
+print "Number of collisions:"
+print len(collisions)
+
+for collision in collisions:
+
+    #cms.pretty_print(collision)
+
+    jets,topjets,muons,electrons,photons,met = collision
+
+    ##################################################################
+    #### Find 2 jets to look for hadronicly decaying W
+    ##################################################################    
+
+    njets = len(jets)
+    if njets>=2:
+        for i in range(0,njets):
+            for j in range(i+1,njets):
+
+                mass=np.sqrt(((jets[i][0]+jets[j][0])**2)-((jets[i][1]+jets[j][1])**2+(jets[i][2]+jets[j][2])**2+(jets[i][3]+jets[j][3])**2))
+                invarient_mass_W.append(mass)
+
+    ##################################################################
+    #### Find 2 same leptons to look for Z
+    ##################################################################    
+    nelectrons=len(electrons)
+    nmuons=len(muons)
+    if nelectrons>=2:
+        for i in range(0,nelectrons):
+            for j in range (i+1,nelectrons):
+                if electrons[i][4]!=electrons[j][4]:
+                    mass=np.sqrt(((electrons[i][0]+electrons[j][0])**2)-((electrons[i][1]+electrons[j][1])**2+(electrons[i][2]+electrons[j][2])**2+(electrons[i][3]+electrons[j][3])**2))
+                    invarient_mass_Z.append(mass)
+                   
+
+    if nmuons>=2:
+        for i in range(0,nmuons):
+            for j in range (i+1,nmuons):
+                if muons[i][4]!=muons[j][4]:
+                    mass=np.sqrt(((muons[i][0]+muons[j][0])**2)-((muons[i][1]+muons[j][1])**2+(muons[i][2]+muons[j][2])**2+(muons[i][3]+muons[j][3])**2))
+                    invarient_mass_Z.append(mass)
+                        
+        
+         
+
+
+
+
+
+
+
+
+
+
+
+plt.figure(1)
+
+
+#plt.hist(invarient_mass_W,bins=150,range=(0,400))
+lkn.hist_err(invarient_mass_W,bins=200)
+plt.xlim([0,400])
+name = r"Hadronic Decay of W $%s$" % (sys.argv[1].split('/')[-1])
+plt.title(name)
+plt.ylabel(r"Frequency")
+plt.xlabel(r"Invariant Mass")
+
+plt.figure(2)
+
+
+#plt.hist(invarient_mass_Z,bins=100,range=(0,250))
+lkn.hist_err(invarient_mass_Z,bins=200)
+plt.xlim([0,400])
+name = r"Decay of Z $%s$" % (sys.argv[1].split('/')[-1])
+plt.title(name)
+plt.ylabel(r"Frequency")
+plt.xlabel(r"Invariant Mass")
+
+
+plt.show()
+
+
